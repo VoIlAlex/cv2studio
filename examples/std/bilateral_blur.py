ece4a36f07434babc0a96c573fbf4dbc2a6a937c
ilya <ilya.vouk@gmail.com>
2019-08-11 15:46:07 +0300

    Add BilateralBlur example

---
+++ examples/std/bilateral_blur.py
@@ -0,0 +1,13 @@
+from cv2studio import App, Component, VIDEO
+from cv2studio.components import BilateralBlur
+
+
+class VideoApp(App):
+    def __init__(self, path):
+        App.__init__(self, path, VIDEO)
+        # self.add_component(MyComponent())
+        self.add_component(BilateralBlur())
+
+
+app = VideoApp('../../res/forest.mp4')
+app.main_loop()
