973897e80b9f734cc35c80acc514af4f3dae9934
ilya <ilya.vouk@gmail.com>
2019-08-11 15:22:15 +0300

    Add kwargs and defaults to GaussianBlur

--- cv2studio/components/blur_components.py
+++ cv2studio/components/blur_components.py
@@ -3,10 +3,18 @@ import cv2
 
 
 class GaussianBlur(Component):
-    def __init__(self):
+    default_ksize = (5, 5)
+    default_sigmaX = 3
+
+    def __init__(self, **kwargs):
         Component.__init__(self)
-        self.state['ksize'] = (5, 5)
-        self.state['sigmaX'] = 3
+
+        # extracting attributes
+        ksize = kwargs.get('ksize', GaussianBlur.default_ksize)
+        sigmaX = kwargs.get('sigmaX', GaussianBlur.default_sigmaX)
+
+        self.state['ksize'] = ksize
+        self.state['sigmaX'] = sigmaX
 
     def process(self, img):
         result = cv2.GaussianBlur(img, self.state['ksize'], self.state['sigmaX'])
