e30ed17d0e026a02964c062a0bc87f10425188d5
ilya <ilya.vouk@gmail.com>
2019-08-11 15:06:04 +0300

    Add GaussianBlur and create blur_components.py

--- cv2studio/components/blur_components.py
+++ cv2studio/components/blur_components.py
@@ -1,1 +1,13 @@
+from .component import Component
+import cv2
 
+
+class GaussianBlur(Component):
+    def __init__(self):
+        Component.__init__(self)
+        self.state['ksize'] = (5, 5)
+        self.state['sigmaX'] = 3
+
+    def process(self, img):
+        result = cv2.GaussianBlur(img, self.state['ksize'], self.state['sigmaX'])
+        return result
