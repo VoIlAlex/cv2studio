50a92b305d55ea99c24bb66a0f3034ae920fcb4e
ilya <ilya.vouk@gmail.com>
2019-08-11 14:20:44 +0300

    Move Component to a separate file

---
+++ cv2studio/components/component.py
@@ -0,0 +1,38 @@
+import cv2
+
+
+class Component(object):
+    '''
+    Component is a part of image processing app.
+    '''
+    def __call__(self, img):
+        """
+        Additional proxy layer between
+        application and component.
+        :param img: image to process
+        :return: processed image
+        """
+
+        # auto layer tuning
+        # Supported formats
+        # - BGR
+        # - GRAY
+        try:
+            result = self.process(img)
+        except Exception:
+            if len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            elif len(img.shape) == 2:
+                img = cv2.merge((img, img, img))
+            result = self.process(img)
+
+        return result
+
+    def process(self, img):
+        '''
+        Core of a component.This method must be
+        implemented in a child-class.
+        :param img: image to be precessed
+        :return: processed image
+        '''
+        raise NotImplementedError('process(img) is not implemented.')
